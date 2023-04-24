035706c67f256b57a4befada722113ee232582df
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-04-24 14:10:28 +0000

    Update master.js

--- master.js
+++ master.js
@@ -1,6 +1,30 @@
 const products = [
   {
-    name: 'MASTER european Professionnelle en COACHING',  
+    name: 'MASTER european  en COACHING',  
+    firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
+  
+  },
+    {
+    name: 'MASTER europeanen MANAGEMENT DES AFFAIRES',  
+    firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
+  
+  },
+      {
+    name: 'MASTER europeanen MARKETING DIGITAL',  
+    firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
+  
+  },
+       {
+    name: 'MASTER europeanen SUPPLY CHAINL',  
+    firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
+  
+  },
+       {
+    name: 'MASTER europeanen LEAN MANAGEMENT',  
     firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
     secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
   
